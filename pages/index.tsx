4ec038d34bb34ed33a8cf3648d93e3be8f850ddd
KeitoIsurugi <isurugi@csplan.jp>
2023-01-21 16:58:24 +0900

    Tailwind CSS導入

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,12 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Hello World!</h1>
+      <h1 className="text-6xl font-bold">
+        Welcome to{' '}
+        <a className="text-blue-600" href="https://nextjs.org">
+          Next.js!
+        </a>
+      </h1>
     </>
   )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  darkMode: false, // 'media' or 'class'
+  purge: {
+    content: [
+      './pages/**/*.{js,ts,jsx,tsx}',
+    ],
+    options: {
+      // https://purgecss.com/safelisting.html#patterns
+      safelist: {
+        standard: [/^bg-/, /^text-/],
+      },
+    },
+  },
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
