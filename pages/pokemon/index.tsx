358408a01f33a02e04c4a3e006d4a3b17235557c
keito-isurugi <ja224kg@gmail.com>
2023-02-03 13:15:30 +0900

    初代ポケモンをカードで一覧表示＆モーダルで詳細表示

--- pages/pokemon/index.tsx
+++ pages/pokemon/index.tsx
@@ -1,14 +1,29 @@
 import { useEffect, useState } from 'react'
 import Axios from 'axios'
-import { resolve } from 'path';
-import { rejects } from 'assert';
-import async from '../api/todo/register';
+import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import Data from '@/lib/json/data.json'; // 追加
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import pokemonJson from '@/lib/json/pokemon.json';
 
 export default function Home() {
 	const [datas, setDatas] = useState<any[]>([])
-
+	const [pokeNum, setPokeNum] = useState<Number>(0)
+	const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+	const style = {
+		position: 'absolute' as 'absolute',
+		top: '50%',
+		left: '50%',
+		transform: 'translate(-50%, -50%)',
+		width: 400,
+		// bgcolor: 'background.paper',
+		border: '1px solid #000',
+		boxShadow: 24,
+		p: 4,
+	};
+	
 	const fetchPokeSpecies = (id: number) => {
 		return new Promise((resolve, rejects) => {
 			Axios
@@ -51,7 +66,7 @@ export default function Home() {
 	
 	const main = async() => {
 		let pokeDatas = [];
-		await Promise.all([...Array(3)].map(async(_, i) => {
+		await Promise.all([...Array(151)].map(async(_, i) => {
 			// 日本語情報取得用データ
 			let pokeSpecies = await fetchPokeSpecies(i + 1)
 			// 詳細情報取得用でーた
@@ -71,6 +86,7 @@ export default function Home() {
 	}
 
 	const pokeDatasSet = async(pokeDatas) => {
+		pokeDatas.sort((a, b) => a.no - b.no);
 		setDatas(pokeDatas)
 	}
 
@@ -110,8 +126,9 @@ export default function Home() {
 			let special_attack = poke.stats[3].base_stat
 			let special_defense = poke.stats[4].base_stat
 			let speed = poke.stats[5].base_stat
+			// 画像
 			let img = poke.sprites.other["official-artwork"].front_default
-			let images = poke.sprites
+			// let images = poke.sprites
 
 			// 出力
 			let poke_json = {
@@ -132,7 +149,7 @@ export default function Home() {
 					speed: speed,
 				},
 				img: img,
-				images: images
+				// images: images
 			}
 			return poke_json
 	}
@@ -147,13 +164,62 @@ export default function Home() {
 		link.click();
 	}
 
+	const setPokeDetail = (i) => {
+		console.log(i)
+		setPokeNum(i)
+		handleOpen()
+	}
+
   return (
     <>
-			<Button className='bg-red-900 hover:bg-red-700' onClick={() => onCliciMain()}>pk</Button>
-			<Button className='bg-blue-900 hover:bg-blue-700' onClick={() => fileDl()}>File</Button>
-			<div>
-				{datas.map((data,index) => (
-					<p key={index}>{data.name}</p>
+			<Button onClick={handleOpen}>Open modal</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        {/* <Box sx={style}> */}
+					<div className="rounded overflow-hidden shadow-lg bg-white" style={style}>
+						<div className="w-full mx-auto bg-gray-300">
+							<img className="w-full" src={pokemonJson[pokeNum].img} alt={pokemonJson[pokeNum].name} />	
+						</div>
+						<div className="px-3 py-2">
+							<dl className='flex flex-wrap mb-2'>
+								<dt className='font-bold text-xl'>No.{pokemonJson[pokeNum].no}：</dt>
+								<dd className='font-bold text-xl'>{pokemonJson[pokeNum].name}</dd>
+							</dl>
+							<dl className='flex flex-wrap mb-2'>
+								<dt className='font-bold text-xl'>分類：</dt>
+								<dd className='font-bold text-xl'>{pokemonJson[pokeNum].classification}</dd>
+							</dl>
+							<dl className='flex flex-wrap mb-2'>
+								<dt className='font-bold text-xl'>タイプ：</dt>
+								<dd className='font-bold text-xl'>{pokemonJson[pokeNum].type1}、{pokemonJson[pokeNum].type2}</dd>
+							</dl>
+							<dl className='flex flex-wrap mb-2'>
+								<dt className='font-bold text-xl'>高さ：{pokemonJson[pokeNum].height / 10}m　重さ：{pokemonJson[pokeNum].weight}kg</dt>
+							</dl>
+							<dl className='flex mb-2'>
+								<dt className='w-3/6 font-bold text-xl'>説明：</dt>
+								<dd className='font-bold text-xl'>{pokemonJson[pokeNum].flavor_text}</dd>
+							</dl>
+						</div>
+					</div>
+        {/* </Box> */}
+      </Modal>
+			{/* <Button className='bg-red-900 hover:bg-red-700' onClick={() => onCliciMain()}>pk</Button>
+			<Button className='bg-blue-900 hover:bg-blue-700' onClick={() => fileDl()}>File</Button> */}
+			<div className='flex gap-4 flex-wrap justify-center'>
+				{pokemonJson.map((data, index) => (
+					<div className="w-1/6 rounded overflow-hidden shadow-lg">
+						<div className="w-full mx-auto bg-gray-300 cursor-pointer" onClick={() => setPokeDetail(index)}>
+							<img className="w-full" src={data.img} alt={data.name} />	
+						</div>
+						<div className="px-2 py-2">
+							<div className="font-bold text-xl text-center">No.{data.no}：{data.name}</div>
+						</div>
+					</div>
 				))}
 			</div>
     </>
